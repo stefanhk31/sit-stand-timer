cff6031d32fac0b1679545122b4368680e17f033
Stefan <shodgeskluck@gmail.com>
2019-02-18 13:57:34 -0500

    Updates

---
+++ precache-manifest.c4eea28245939bdccd1e7eb1d3babbfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb3f4b9da9a90ef6e09b",
+    "url": "/sit-stand-timer/static/js/runtime~main.bb3f4b9d.js"
+  },
+  {
+    "revision": "b1a836fedc96cc4213cb",
+    "url": "/sit-stand-timer/static/js/main.b1a836fe.chunk.js"
+  },
+  {
+    "revision": "3d51242093a00e5dcfca",
+    "url": "/sit-stand-timer/static/js/2.3d512420.chunk.js"
+  },
+  {
+    "revision": "b1a836fedc96cc4213cb",
+    "url": "/sit-stand-timer/static/css/main.8aa9e12f.chunk.css"
+  },
+  {
+    "revision": "4f913a4f6db36f7917b420a903cffc50",
+    "url": "/sit-stand-timer/index.html"
+  }
+];
